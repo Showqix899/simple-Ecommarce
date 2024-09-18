b6b5d7df84b62a9bec7af07168d4a9b48e11327b
showqi <mshowqi12@gmail.com>
2024-09-18 17:48:58 +0000

    only the models and the home page added

---
+++ ecom/store/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import *
+
+admin.site.register(Catagory)
+admin.site.register(Customer)
+admin.site.register(Products)
+admin.site.register(Order)
---
+++ ecom/store/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+import datetime
+
+# Create your models here.
+#catagories
+class Catagory(models.Model):
+    name=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+#customers
+class Customer(models.Model):
+
+    first_name=models.CharField(max_length=100)
+    last_name=models.CharField(max_length=100)
+    phone=models.CharField(max_length=11)
+    email=models.EmailField(max_length=100)
+    password=models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return f'{self.first_name} + {self.last_name}'
+#products
+
+class Products(models.Model):
+    name=models.CharField(max_length=100)
+    price=models.DecimalField(default=0,decimal_places=2,max_digits=6)
+    catagory=models.ForeignKey(Catagory,on_delete=models.CASCADE,default=1)
+    description=models.TextField(max_length=250,default='',null=True,blank=True)
+    image=models.ImageField(upload_to='uploads/products/')
+
+    def __str__(self):
+        return self.name
+
+#order
+class Order(models.Model):
+    product=models.ForeignKey(Products,on_delete=models.CASCADE)
+    customer=models.ForeignKey(Customer,on_delete=models.CASCADE)
+    quntity=models.IntegerField(default=1)
+    address=models.CharField(max_length=200,blank=True,default='')
+    phone=models.CharField(max_length=11,blank=True,default='')
+    date=models.DateField(default=datetime.datetime.today)
+    status=models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.product
+
+
+    
