8f1e6fa7ce959e199c5fb1c075a547843b894709
showqi <mshowqi12@gmail.com>
2024-09-19 11:55:15 +0000

    login fuctionality imported and templates are staged

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Products
-
+from django.contrib.auth import logout,login,authenticate
 # Create your views here.
 
 #view for home page
 def home(request):
     products=Products.objects.all() # fetching all the products from the database
     return render (request,'index.html',{'products':products})
+
+#about page
+
+def about(request):
+
+    return render(request,'about.html',{})
+
+def login_user(request):
+    return render(request,'login.html',{})
